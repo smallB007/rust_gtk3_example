b3d8301cfb06cb374da8cbe4eba4d65d4c9fd24e
artie <atch.cpp@gmail.com>
2022-03-28 18:50:59 +0200

    Print:)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
-
 fn main() {
-    let app = Application::builder()
-        .application_id("org.example.HelloWorld")
-        .build();
-
-    app.connect_activate(|app| {
-        // We create the main window.
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(200)
-            .title("Hello, Rust World!")
-            .build();
-
-        // Don't forget to make all widgets visible.
-        win.show_all();
-    });
-
-    app.run();
+    println!("Hello from rust Artie!");
 }
